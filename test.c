35a3ac31e0b7a13915634744ab989e897132dbf8
Fadhili Njagi <fadhilinjagi@gmail.com>
2022-05-14 18:34:22 +0300

    Program runs but logical testing is still ongoing

---
+++ test.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+  char arr[2][20] = {"Hey"};
+  printf("%i", strlen(arr[1]));
+//  if (arr[1] == NULL) {
+//  	printf("\nYaay");
+//  }
+   
+   return(0);
+}
